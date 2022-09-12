59016e81cd9c7902ef5ff60945a5f7d143e180b8
Leonardo Lazaro Barboza <leonardo_laz@outlook.com>
2022-09-12 05:11:42 -0300

    Update 063 - Sequência de Fibonacci v1.0.py

--- CursoemVideoPython/063 - Sequência de Fibonacci v1.0.py
+++ CursoemVideoPython/063 - Sequência de Fibonacci v1.0.py
@@ -1,1 +1,19 @@
+from ast import Num
 
+
+print('-' * 50)
+print('Sequência de Fibonacci')
+print('-' * 50)
+termos = int(input('Quantos termos você quer mostrar? '))
+print('~' * 50)
+x = cont = 0
+y = 1
+while cont < termos:
+    print(x, end=' -> ')
+    cont += 1
+    z = x + y
+    x = y
+    y = z
+print('FIM', end='')
+print()
+print('~' * 50)
