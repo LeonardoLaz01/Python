3e3ca8ac49187528e929d84fd01ed4410236500c
Lazarett0 <leonardo_laz@outlook.com>
2022-10-01 15:22:23 -0300

    .

--- CursoemVideoPython/modulos115.py
+++ CursoemVideoPython/modulos115.py
@@ -4,6 +4,25 @@ def interface(msg):
     print('-' * 40)
 
 
+def verpessoas():
+    try:
+        arquivo = open('cadastro.txt', 'r')
+    except:
+        arquivo = open('cadastro.txt', 'wt+')
+    else:
+        for linha in arquivo:
+            dado = linha.split(';')
+            dado[1] = dado[1].replace('\n', '')
+            print(f'{dado[0]:<30}{dado[1]:>3} anos')
+
+
+def cadastropessoas():
+    arquivo = open('cadastro.txt', 'a')
+    nome = str(input('Nome: '))
+    idade = int(input('Idade: '))
+    arquivo.write(f'{nome};{idade}\n')
+
+
 def menu():
     while True:
         interface('MENU PRINCIPAL')
@@ -15,9 +34,11 @@ def menu():
             print('ERRO: por favor, digite um número inteiro válido')
         else:
             if op == 1:
-                interface(f'Opção {op}')
+                interface('PESSOAS CADASTRADAS')
+                verpessoas()
             elif op == 2:
-                interface(f'Opção {op}')
+                interface('NOVO CADASTRO')
+                cadastropessoas()
             elif op == 3:
                 interface(f'Saindo do sistema...até logo!')
                 break
